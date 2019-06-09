8648dfe880813cc4d43d8d115cdb02b5c2888141
xu <xu@qq.com>
2019-06-09 11:42:08 +0800

    抽取项目的配置到单独的文件

---
+++ config.py
@@ -0,0 +1,26 @@
+from redis import StrictRedis
+
+class Config(object):
+    '''项目的配置'''
+    DEBUG = True
+
+    SECRET_KEY = "Wub8dLQfSWgqpyEkUieJVzYX1bVY61/cE1npjbJhlzm57qTPpnDzn3Yvu2j2DMGU"
+
+    #数据库添加配置
+    SQLALCHEMY_DATABASE_URI= "mysql://root:mysql@127.0.0.1:3306/website"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    #Redis 的 配置
+    REDIS_HOST = "127.0.0.1"
+    REDIS_PORT = 6379
+
+    #Session保存配置
+    SESSION_TYPE = "redis"
+    #开启session签名
+    SESSION_USE_SIGNER = True
+    #制定Session 保存的 redis
+    SESSION_REDIS = StrictRedis(host= REDIS_HOST,port=REDIS_PORT)
+    #设置session需要过期
+    SESSION_PERMANENT = False
+    #设置过期时间
+    PERMANENT_SESSION_LIFETIME = 86400 * 2
--- news_website.py
+++ news_website.py
@@ -6,35 +6,7 @@ from flask_wtf.csrf import CSRFProtect
 #可以用来指定session保存位置
 from flask_session import Session
 from flask_migrate import Migrate,MigrateCommand
-
-
-class Config(object):
-    '''项目的配置'''
-    DEBUG = True
-
-    SECRET_KEY = "Wub8dLQfSWgqpyEkUieJVzYX1bVY61/cE1npjbJhlzm57qTPpnDzn3Yvu2j2DMGU"
-
-    #数据库添加配置
-    SQLALCHEMY_DATABASE_URI= "mysql://root:mysql@127.0.0.1:3306/website"
-    SQLALCHEMY_TRACK_MODIFICATIONS = Flask
-
-    #Redis 的 配置
-    REDIS_HOST = "127.0.0.1"
-    REDIS_PORT = 6379
-
-    #Session保存配置
-    SESSION_TYPE = "redis"
-    #开启session签名
-    SESSION_USE_SIGNER = True
-    #制定Session 保存的 redis
-    SESSION_REDIS = StrictRedis(host= REDIS_HOST,port=REDIS_PORT)
-    #设置session需要过期
-    SESSION_PERMANENT = False
-    #设置过期时间
-    PERMANENT_SESSION_LIFETIME = 86400 * 2
-
-
-
+from config import Config
 
 app = Flask(__name__)
 app.config.from_object(Config)
