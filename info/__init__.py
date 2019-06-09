51befd8aaf6bdb0f0e1eee6acdf42b0482080437
xu <xu@qq.com>
2019-06-09 15:11:58 +0800

    通过配置名字获取指定的配置

--- info/__init__.py
+++ info/__init__.py
@@ -7,14 +7,22 @@ from flask_wtf.csrf import CSRFProtect
 from flask_session import Session
 from config import config
 
-app = Flask(__name__)
-app.config.from_object(config["development"])
-
 #初始化数据库
-db = SQLAlchemy(app)
-#初始化redis 存储对象
-redis_store = StrictRedis(host = config["development"].REDIS_HOST,port = config["development"].REDIS_PORT)
-#开启当前项目CSRF 保护,只做服务器验证
-CSRFProtect(app)
-#设置Session保存指定位置
-Session(app)
+#在flask里很多扩展都可以先初始化扩展的对象,然后再去调用init_app方法去初始化
+db = SQLAlchemy()
+
+
+def create_app(config_name):
+    app = Flask(__name__)
+    app.config.from_object(config[config_name])
+
+    #初始化数据库
+    db.init_app(app)
+    #初始化redis 存储对象
+    redis_store = StrictRedis(host = config[config_name].REDIS_HOST,port = config[config_name].REDIS_PORT)
+    #开启当前项目CSRF 保护,只做服务器验证
+    CSRFProtect(app)
+    #设置Session保存指定位置
+    Session(app)
+
+    return app
--- news_website.py
+++ news_website.py
@@ -2,10 +2,13 @@ from flask import session
 from flask_script import Manager
 from flask_migrate import Migrate,MigrateCommand
 
-from info import app,db
+from info import db, create_app
 
-
+#通过指定的配置名字创建对应配置的app
+#工厂方法
+app = create_app("development")
 manager = Manager(app)
+
 #将app与db关联
 Migrate(app,db)
 #将迁移命令添加到manager中
