3f48f56a077e42787845b544e4396cf391edcc77
xu <xu@qq.com>
2019-06-11 15:31:30 +0800

    图片验证码后端逻辑实现

--- info/static/news/js/main.js
+++ info/static/news/js/main.js
@@ -148,14 +148,14 @@ $(function(){
     })
 })
 
-var imageCodeId = ""
+var imageCodeId = "";
 
 // 生成一个图片验证码的编号，并设置页面中图片验证码img标签的src属性
 function generateImageCode() {
     //浏览器要发起图片验证码请求/image_code?imageCodeId=xxxxxx
-    imageCodeId = generateUUID()
+    imageCodeId = generateUUID();
     //生成url
-    var url ="image_code?imageCodeI="+imageCodeId
+    var url ="image_code?imageCodeId="+imageCodeId;
     //给指定img标签设置src,设置了地址之后,img标签就会去向这个地址发起请求
     $(".get_pic_code").attr("src",url)
 }
