361aa0cfac43a98102447b10534a66ba32c9e203
xu <xu@qq.com>
2019-06-11 10:51:41 +0800

    加载favicon.ico图标

--- info/modules/index/views.py
+++ info/modules/index/views.py
@@ -1,3 +1,4 @@
+from flask import current_app
 from flask import render_template
 
 from info import redis_store
@@ -6,3 +7,9 @@ from . import index_blu
 def hello_world():
     #向redis中保存一个值
     return render_template("news/index.html")
+
+#加载favicon.ico图标
+#send_static_file是flask把静态文件从静态文件夹发送到浏览器
+@index_blu.route("/favicon.ico")
+def favicon():
+    return current_app.send_static_file("news/favicon.ico")
