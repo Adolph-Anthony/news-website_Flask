db987df64d8c419f05c5287cef8c5a52cb074285
Athony.xu <51499806+Adolph-Anthony@users.noreply.github.com>
2019-06-20 11:12:38 +0800

    个人中心基类模版抽取

--- info/modules/passport/__init__.py
+++ info/modules/passport/__init__.py
@@ -1,3 +1,4 @@
+#登录注册的相关业务逻辑都放在当前模块
 from flask import Blueprint
 
 #创建蓝图对象
---
+++ info/modules/profile/__init__.py
@@ -0,0 +1,6 @@
+#存放个人中心逻辑
+from flask import Blueprint
+
+#创建蓝图对象
+profile_blu = Blueprint("profile",__name__,url_prefix="/user")
+from . import views
