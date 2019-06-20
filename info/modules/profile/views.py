05831e149a6f6456ff5e80b87ac9583fa3e6d38c
Athony.xu <51499806+Adolph-Anthony@users.noreply.github.com>
2019-06-20 18:49:05 +0800

    上传头像的前端代码实现

--- info/modules/profile/views.py
+++ info/modules/profile/views.py
@@ -1,27 +1,44 @@
+from flask import current_app
 from flask import g, jsonify
 from flask import redirect
 from flask import render_template
 from flask import request
 
+from info import constants
 from info.modules.profile import profile_blu
+from info.templates.image_storage import storage
 from info.utils.common import user_login_data
 from info.utils.response_code import RET
 
-@profile_blu.route("/pic_info",methods= ["POST","GET"])
+
+@profile_blu.route("/pic_info", methods=["POST", "GET"])
 @user_login_data
 def pic_info():
+    user = g.user
     if request.method == "GET":
         data = {
 
-            "user": g.user.to_dict()
+            "user": user.to_dict()
         }
-        return  render_template("news/user_pic_info.html",data = data)
-    # TODO 如果是 POST表示修改头像
+        return render_template("news/user_pic_info.html", data=data)
+    # 如果是 POST表示修改头像
+    # 1.取到上传的图片
+    try:
+        avatar = request.files.get("avatar").read()
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.PARAMERR, errmsg="参数错误")
+    # 2.上传图片
+    try:
+        key = storage(avatar)
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.THIRDERR, errmsg="上传图片失败")
 
+    user.avatar_url = key
+    return jsonify(errno = RET.OK,errmsg = "OK",avatar_url = constants.QINIU_DOMIN_PREFIX + key )
 
-
-
-@profile_blu.route("/base_info",methods= ["POST","GET"])
+@profile_blu.route("/base_info", methods=["POST", "GET"])
 @user_login_data
 def base_info():
     if request.method == "GET":
@@ -29,7 +46,7 @@ def base_info():
 
             "user": g.user.to_dict()
         }
-        return render_template("news/user_base_info.html",data=data)
+        return render_template("news/user_base_info.html", data=data)
 
     '''
     修改用户数据
@@ -49,19 +66,19 @@ def base_info():
     signature = request.json.get("signature")
     gender = request.json.get("gender")
 
-    #2.校验参数
-    if not all([nick_name,signature,gender]):
-        return jsonify(errno = RET.PARAMERR,error = "参数错误")
-    if gender not in (["MAN","WOMEN"]):
-        return jsonify(errno = RET.PARAMERR,error = "参数错误")
-    #保存参数
+    # 2.校验参数
+    if not all([nick_name, signature, gender]):
+        return jsonify(errno=RET.PARAMERR, error="参数错误")
+    if gender not in (["MAN", "WOMEN"]):
+        return jsonify(errno=RET.PARAMERR, error="参数错误")
+    # 保存参数
     user = g.user
     user.signature = signature
     user.nick_name = nick_name
     user.gender = gender
-    #user已经存在并且会自动commit()
+    # user已经存在并且会自动commit()
 
-    return jsonify(errno = RET.OK,errmsg = "OK")
+    return jsonify(errno=RET.OK, errmsg="OK")
 
 
 @profile_blu.route("/info")
@@ -75,6 +92,6 @@ def user_info():
 
     data = {
 
-        "user":user.to_dict()
+        "user": user.to_dict()
     }
-    return render_template("news/user.html",data = data)
+    return render_template("news/user.html", data=data)
