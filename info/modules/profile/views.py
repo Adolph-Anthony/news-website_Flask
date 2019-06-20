bfcfe5e01d3bb5727b6fc829762f500a1c423f25
Athony.xu <51499806+Adolph-Anthony@users.noreply.github.com>
2019-06-20 14:48:47 +0800

    基本资料界面展示

--- info/modules/profile/views.py
+++ info/modules/profile/views.py
@@ -1,9 +1,24 @@
 from flask import g
 from flask import redirect
 from flask import render_template
+from flask import request
 
 from info.modules.profile import profile_blu
 from info.utils.common import user_login_data
+
+
+@profile_blu.route("/base_info",methods= ["POST","GET"])
+@user_login_data
+def base_info():
+    if request.method == "GET":
+        data = {
+
+            "user": g.user.to_dict()
+        }
+        return render_template("news/user_base_info.html",data=data)
+    # TODO 修改用户数据
+    if request.method == "POST":
+        pass
 
 
 @profile_blu.route("/info")
